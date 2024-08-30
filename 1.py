ca365f0e9d8f7141775f9ccf0eb3718126bac532
balamurugansceg <balamurugansceg@gmail.com>
2024-08-30 11:32:11 +0530

    Create 1.py
    Practice

---
+++ 1.py
@@ -0,0 +1,6 @@
+import math
+def area_of_circle(radius):
+    return math.pi * radius **2
+radius = int(input("Enter the radius : "))
+area = area_of_circle(radius)
+print("The area of the circle is :", area)
